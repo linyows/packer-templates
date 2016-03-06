4711f683f9cafa61320e0c95e5fbfb7c909fbb95
linyows <linyows@gmail.com>
2016-03-06 16:50:48 +0900

    fix for network

--- scripts/centos/cleanup.sh
+++ scripts/centos/cleanup.sh
@@ -7,6 +7,9 @@ rm -f /tmp/chef*rpm
 
 # clean up redhat interface persistence
 rm -f /etc/udev/rules.d/70-persistent-net.rules
+mkdir -p /etc/udev/rules.d/70-persistent-net.rules;
+rm -f /lib/udev/rules.d/75-persistent-net-generator.rules;
+rm -rf /dev/.udev/;
 
 # remove uploaded_files from packer
 rm -rf /tmp/uploaded_files
